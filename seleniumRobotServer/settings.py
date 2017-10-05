a306959e89a4dd441f477ba1f49a778cd7a0d0a3
s047432 <s047432@SN782980.societe.mma.fr>
2017-10-05 17:18:25 +0200

    added static handling on apache

--- seleniumRobotServer/settings.py
+++ seleniumRobotServer/settings.py
@@ -136,6 +136,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
