257689015bda6e337279c0fa67150d4477471239
s047432 <s047432@SN782980.societe.mma.fr>
2017-10-05 17:17:29 +0200

    Added missing views for admin

--- variableServer/admin.py
+++ variableServer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django import forms
-from variableServer.models import Variable
+from variableServer.models import Variable, TestCase, Application, Version
 from django.template.context_processors import csrf
 from django.shortcuts import render_to_response
 from variableServer.models import TestEnvironment
@@ -188,7 +188,109 @@ class EnvironmentAdmin(admin.ModelAdmin):
     list_filter = ('genericEnvironment', )
     list_display = ('name', 'genericEnvironment')
     form = EnvironmentForm
+    
+    
+class TestCaseForm(forms.ModelForm):
+    
+    def __init__(self, *args, **kwargs):
+        super(TestCaseForm, self).__init__(*args, **kwargs)
+        self.fields['variables'].required = False
+
+class TestCaseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'application')
+    list_filter = ('application',)
+    form = TestCaseForm
+    
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    
+    # deactivate all actions so that deleting an application must be done from detailed view
+    actions = None
+    
+    def get_fieldsets(self, request, obj=None):
+        """
+        Display error message when it's impossible to delete the application
+        """
+        if not (obj and len(TestCase.objects.filter(application=obj)) and len(Variable.objects.filter(application=obj))):
+            return admin.ModelAdmin.get_fieldsets(self, request, obj=obj)
+        
+        # if some variables are linked to this application
+        else:
+            return (
+                 (None, {
+                        'fields': ('name',),
+                        'description': '<div style="font-size: 16px;color: red;">All tests / variables must be deleted before this application can be deleted</div>'}
+                  ),
+                 )
+               
+    def has_delete_permission(self, request, obj=None):
+        """
+        Do not display delete button if some tests / variables are linked to this application
+        """
+        
+        canDelete = admin.ModelAdmin.has_delete_permission(self, request, obj=obj)
+        
+        # when no object provided, default behavior
+        if not obj:
+            return canDelete
+        
+        # check for linked test cases and variables
+        if len(TestCase.objects.filter(application=obj)) and len(Variable.objects.filter(application=obj)):
+            return False
+        else:
+            return True and canDelete 
+        
+class VersionForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(VersionForm, self).__init__(*args, **kwargs)
+        self.fields['linkedVersions'].required = False
+        
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'application')
+    list_filter = ('application',)
+    form = VersionForm
+    
+    # deactivate all actions so that deleting a version must be done from detailed view
+    actions = None
+    
+    def get_fieldsets(self, request, obj=None):
+        """
+        Display error message when it's impossible to delete the version
+        """
+        if not (obj and len(Variable.objects.filter(version=obj))):
+            return admin.ModelAdmin.get_fieldsets(self, request, obj=obj)
+        
+        # if some variables are linked to this application
+        else:
+            return (
+                 (None, {
+                        'fields': ('name', 'application', 'linkedVersions'),
+                        'description': '<div style="font-size: 16px;color: red;">This version will be deleted when all linked variables will be deleted</div>'}
+                  ),
+                 )
+            
+            
+            
+    def has_delete_permission(self, request, obj=None):
+        """
+        Do not display delete button if some tests / variables are linked to this application
+        """
+        
+        canDelete = admin.ModelAdmin.has_delete_permission(self, request, obj=obj)
+        
+        # when no object provided, default behavior
+        if not obj:
+            return canDelete
+        
+        # check for linked variables
+        if len(Variable.objects.filter(version=obj)):
+            return False
+        else:
+            return True and canDelete    
 
 # Register your models here.
+admin.site.register(Application, ApplicationAdmin)
+admin.site.register(TestCase, TestCaseAdmin)
+admin.site.register(Version, VersionAdmin)
 admin.site.register(Variable, VariableAdmin)
 admin.site.register(TestEnvironment, EnvironmentAdmin)
