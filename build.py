a22d27cdeb7430bab7bf609d1ab4568589cc8b95
s047432 <s047432@SN782980.societe.mma.fr>
2017-10-05 17:18:00 +0200

    create build script

---
+++ build.py
@@ -0,0 +1,78 @@
+import os
+import shutil
+from zipfile import ZipFile, ZIP_DEFLATED, ZipInfo
+from seleniumRobotServer.settings import STATIC_ROOT
+
+root = os.path.abspath(os.path.dirname(__file__))
+distDir = root + os.sep + 'dist'
+
+"""
+deployment process:
+- python install
+- apache install
+    - apache from apachelounge, same bitness as python
+    - C++ redistributable microsoft, same version as the one used for apache compilation
+    - mod_wsgi, same bitness as python
+- install python requirements
+- deploy files
+- database migration
+- create super user on first deploy
+"""
+
+def getFileList():
+    distFileList = []
+    for dirName, subdirList, fileList in os.walk(root):
+        
+        # skip media files
+        if os.path.normpath(dirName).endswith("media/documents"):
+            distFileList.append(dirName)
+            continue
+        
+        subdirList[:] = [d for d in subdirList if not (d.startswith(".") 
+                                                    or d.startswith("__") 
+                                                    or (d == 'documents' and os.path.basename(dirName) == 'media')
+                                                    )]
+        
+        for fileName in fileList:
+            if os.path.splitext(fileName)[1].lower() in [".py", ".md", ".txt", ".yaml", ".css", ".js", ".png", ".jpg", ".html"]:
+                distFileList.append(dirName + os.sep + fileName)
+                
+    return distFileList
+
+def collectStatic():
+    shutil.rmtree(STATIC_ROOT, ignore_errors=True)
+    
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seleniumRobotServer.settings")
+    from django.core.management import execute_from_command_line
+    execute_from_command_line([root + "/manage.py", "collectstatic"])
+                
+def makeDist(fileList):
+    
+    os.makedirs(distDir, exist_ok=True)
+    for f in fileList:
+        copyTo = f.replace(root, distDir)
+        os.makedirs(os.path.dirname(copyTo), exist_ok=True)
+        shutil.copy(f, copyTo)
+        
+    # add media dir
+    os.makedirs(os.path.join(distDir, 'media', 'documents'))
+    
+def createDistFile(fileList): 
+    with ZipFile('seleniumrobot-server.zip', 'w') as srsZip:
+        for f in fileList:
+            srsZip.write(f, f.replace(root, '', ZIP_DEFLATED))
+        zfi = ZipInfo('logs/')
+        srsZip.writestr(zfi, '')
+        zfi = ZipInfo('media/documents/')
+        srsZip.writestr(zfi, '')
+     
+        
+if __name__ == '__main__':
+    
+    shutil.rmtree(distDir, ignore_errors=True)
+    
+    collectStatic()
+    fileList = getFileList()
+    makeDist(fileList)
+    createDistFile(fileList)
+    
