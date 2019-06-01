164cac66310508123217e9968eef5f01b2dfe182
bhecquet <bhecquet.w@free.fr>
2019-06-01 12:39:08 +0200

    issue #34: add test description

--- variableServer/tests/testAdmin.py
+++ variableServer/tests/testAdmin.py
@@ -18,3 +18,7 @@ Created on 28 mars 2018
 #   - cannot add testcase to a restricted application
 #   - cannot add version to a restricted application
 #   - for unrestricted applications, check the above behavior are not active
+# - change multiple variables at once
+# - copy multiple variables
+# - copy several variables, one with tests, other without and check that resulting test is none
+# - copy several variables, all with tests and check that resulting tests are the same as from variables
