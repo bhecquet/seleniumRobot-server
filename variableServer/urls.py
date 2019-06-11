a14b638ec46410da661e9016e2757e61d445f064
bhecquet <bhecquet.w@free.fr>
2019-06-11 15:47:40 +0200

    issue #35: add a way to create or retrieve the auth token via API

--- variableServer/urls.py
+++ variableServer/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from variableServer.views import varActionView, apiView
 from variableServer.views.apiView import VariableList
+from rest_framework.authtoken import views
 
 urlpatterns = [
     url(r'^api/variable/(?P<pk>[0-9]+)/$', VariableList.as_view(), name='variableApiPut'),
     url(r'^api/variable', VariableList.as_view(), name='variableApi'),
-    url(r'^api', apiView.ping, name='variablePing'),
+    url(r'^api', apiView.Ping.as_view(), name='variablePing'),
     
     url(r'copyVariables', varActionView.copyVariables, name='copyVariables'),
     url(r'changeVariables', varActionView.changeVariables, name='changeVariables'),
