a4a753ac185997de3f3a905aba267bda669ed90f
bhecquet <bhecquet.w@free.fr>
2021-06-22 11:42:43 +0200

    issue #95: rename variables

--- variableServer/utils/utils.py
+++ variableServer/utils/utils.py
@@ -3,13 +3,13 @@
 SPECIAL_NONE = '_NONE_'             # used when looking for something where we want an explicit value of None for the parameter
 SPECIAL_NOT_NONE = '_NOT_NONE_'     # used when looking for something where we want an explicit value of 'not None' for the parameter
 
-def updateVariables(sourceQuerySet, additionalQuerySet):
+def updateVariables(source_query_set, additional_query_set):
         """
-        do a queryset union between sourceQuerySet and additionalQuerySet, but when the same variable (same name) exists in both, this is the one from
-        additionalQuerySet which is kept
+        do a queryset union between source_query_set and additional_query_set, but when the same variable (same name) exists in both, this is the one from
+        additional_query_set which is kept
         
         union() is not used as it prevents from doing further combinations
         """
         
-        filteredSourceQuerySet = sourceQuerySet.exclude(name__in=additionalQuerySet.values('name'))
-        return filteredSourceQuerySet | additionalQuerySet
+        filtered_source_query_set = source_query_set.exclude(name__in=additional_query_set.values('name'))
+        return filtered_source_query_set | additional_query_set
